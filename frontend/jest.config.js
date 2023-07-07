53119b57dca6a2ea57be16a9ed5dedda34f27d88
FedMG <75050231+FedMG@users.noreply.github.com>
2023-07-07 03:49:03 +0000

    chore: add new scripts and jest coverage config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,16 +8,24 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   // ignore files in __tests__ directory
+  verbose: 'true',
   modulePathIgnorePatterns: [''],
   moduleDirectories: ['node_modules', '<rootDir>'],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    // '^@/components/(.*)$': '<rootDir>/components/$1',
-    // '^@/pages/(.*)$': '<rootDir>/pages/$1'
-    '^@/(.*)$': '<rootDir>/src/$1',
-    
+    '^@/(.*)$': '<rootDir>/src/$1',    
   },
+  // collectCoverage: true,
+  // skipFull - hide coverage report lines for all fully-covered files
+  // coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*types.d.ts",
+    "!src/**/*schemas.d.ts",
+    "!src/**/*index.ts",
+    "!src/index.ts",
+  ],
   testEnvironment: 'jest-environment-jsdom',
   // snapshotSerializers: ['<rootDir>/src/snapshotSerializer.ts']
 }
