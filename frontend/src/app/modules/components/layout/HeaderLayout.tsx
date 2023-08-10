c5378b6440a677c4b031de44b5d3fc8c296d3c9f
FedMG <75050231+FedMG@users.noreply.github.com>
2023-08-10 16:02:43 +0000

    feat: update styles in HeaderLayout component

--- frontend/src/app/modules/components/layout/HeaderLayout.tsx
+++ frontend/src/app/modules/components/layout/HeaderLayout.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
 
-import { HeaderLogo } from './HeaderLogo'
-import { Header, Navigation } from '../templates'
 import { isValidArray } from '../../utils'
+import { Header, Navigation } from '../templates'
+import HeaderLogo from './HeaderLogo'
 
 const PAGE_ALIGN_BREAKPOINT = 'px-6 sm:px-10 lg:px-16 xl:px-24 py-2.5'
-const PAGE_BG_COLOR = 'bg-gray-100'
 
 export interface HeaderLayoutProps {
   path: string
@@ -14,23 +13,28 @@ export interface HeaderLayoutProps {
 
 export const HeaderLayout: React.FC<{ pages: HeaderLayoutProps[] }> = ({ pages }) => {
   return (
-    <Header labelledby='header-title' className={`${PAGE_BG_COLOR} sticky top-0 z-20`}>
+    <Header labelledby='header-title' className='bg-gray-100 sticky top-0 z-50'>
       <Navigation
         ariaLabel='Main navigation'
-        className={`border-b border-gray-200 ${PAGE_ALIGN_BREAKPOINT}`}
-      >
-        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl gap-2'>
+        className={`border-b border-gray-200 ${PAGE_ALIGN_BREAKPOINT}`}>
+        <div className='flex flex-wrap justify-center items-center mx-auto max-w-screen-xl gap-2 sm:gap-16'>
           <HeaderLogo id='header-title' src='/icon.ico'>
             <span className='text-[#764EF3] font-medium'>Astra</span>
-            <span className='font-semibold'>Dev</span>
+            <span className='font-semibold text-gray-800'>Dev</span>
           </HeaderLogo>
 
-          {isValidArray(pages) &&
-            pages.map(({ path, label }) => (
-              <Link key={label} href={path}>
-                {label}
-              </Link>
-            ))}
+          <ul className='bg-inherit flex flex-row justify-end items-center'>
+            {isValidArray(pages) &&
+              pages.map(({ path, label }) => (
+                <li key={label} className='text-center bg-inherit hover:bg-gray-300 active:bg-gray-200 rounded-md'>
+                  <Link
+                    href={path}
+                    className='py-3 px-7 sm:px-12 block text-gray-800 hover:text-[#764EF3] text-md font-medium border-b border-gray-100 lg:border-0'>
+                    {label}
+                  </Link>
+                </li>
+              ))}
+          </ul>
         </div>
       </Navigation>
     </Header>
