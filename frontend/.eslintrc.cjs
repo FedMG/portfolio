fcfba0a416393d194e37d16b10456c47167b5564
FedMG <75050231+FedMG@users.noreply.github.com>
2023-08-19 20:03:29 +0000

    chore: install and config typescript-eslint
    remove: ts-standard eslint config
    
    update: package.json and sandbox configs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,7 +2,9 @@ module.exports = {
   // rules that applies to global project
   // plugins: ["testing-library", "jest"],
   root: true,
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {},
   overrides: [
     {
