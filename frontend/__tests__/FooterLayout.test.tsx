4a5b22652b5852d9010391d2ba1a38c8e7815831
FedMG <75050231+FedMG@users.noreply.github.com>
2023-10-17 06:33:02 +0000

    test: fix FooterLayout unhandled test
    FolderIcon svg used in FooterLayout component had not been included in test

--- frontend/__tests__/FooterLayout.test.tsx
+++ frontend/__tests__/FooterLayout.test.tsx
@@ -9,6 +9,7 @@ afterEach(() => cleanup)
 jest.mock('@/app/modules/assets', () => ({
   GithubIcon: () => <svg role='img'>Mocked GithubIcon Component</svg>,
   LinkedInIcon: () => <svg role='img'>Mocked LinkedInIcon Component</svg>,
+  FolderIcon: () => <svg role='img'>Mocked FolderIcon Component</svg>,
   // SquareIcon: () => <div/>, // I had not iclude it, why?
   LeftArrowIcon: () => <div />, // I had include it for a warning message
   RightArrowIcon: () => <div /> // I had include it for a warning message
@@ -28,14 +29,14 @@ jest.mock('@/app/modules/components/layout/FooterLink.tsx', () => ({
 
 describe('FooterLayout component', () => {
   describe('WHEN is mounted', () => {
-    it('SHOULD renders a copyright text, and linkedin and github links with an image', async () => {
+    it('SHOULD renders a copyright text, and linkedin, github and folderRepository links with an image', async () => {
       // Arrange
       const copyrightText = '© 2023 AstraDev™ Copyright'
 
       // Act
       render(<FooterLayout />)
       const [github, linkedIn] = screen.getAllByRole('link')
-      const [githubImage, linkedInImage] = screen.getAllByRole('img')
+      const [githubImage, linkedInImage, folderImage] = screen.getAllByRole('img')
 
       // Assert
       expect(screen.getByText(copyrightText)).toBeInTheDocument()
@@ -45,23 +46,30 @@ describe('FooterLayout component', () => {
 
       expect(github).toBeInTheDocument()
       expect(github).toContainElement(githubImage)
+
+      expect(folderImage).toBeInTheDocument()
+      expect(folderImage).toContainElement(folderImage)
     })
 
-    it('SHOULD match with mocked footer, footerLink, githubIcon and LinkedInIcon text', () => {
+    it('SHOULD match with mocked footer, footerLink, githubIcon, LinkedInIcon and folderIcon text', () => {
       // Arrange
       const footerText = 'Mocked Footer Template Component'
       const footerLinkText = 'Mocked FooterLink Component'
       const githubIconText = 'Mocked GithubIcon Component'
       const linkedInIconText = 'Mocked LinkedInIcon Component'
+      const folderIconText = 'Mocked FolderIcon Component'
+      const itemsLength = 3
 
       // Act
       render(<FooterLayout />)
 
       // Assert
       expect(screen.getByText(footerText)).toBeInTheDocument()
-      expect(screen.getAllByText(footerLinkText)).toHaveLength(2)
+      expect(screen.getAllByText(footerLinkText)).toHaveLength(itemsLength)
+
       expect(screen.getByText(githubIconText)).toBeInTheDocument()
       expect(screen.getByText(linkedInIconText)).toBeInTheDocument()
+      expect(screen.getByText(folderIconText)).toBeInTheDocument()
     })
   })
 
@@ -71,11 +79,12 @@ describe('FooterLayout component', () => {
 
       // Act
       render(<FooterLayout />)
-      const [github, linkedIn] = await screen.findAllByRole('link')
+      const [github, linkedIn, portfolioRepository] = await screen.findAllByRole('link')
 
       // Assert
       expect(github).toHaveAttribute('href', github.getAttribute('href'))
       expect(linkedIn).toHaveAttribute('href', linkedIn.getAttribute('href'))
+      expect(portfolioRepository).toHaveAttribute('href', portfolioRepository.getAttribute('href'))
     })
   })
 })
